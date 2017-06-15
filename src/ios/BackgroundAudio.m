b9361a9e1ce7a40564e88edf0abce22fa723b2ab
jerejigga <jerejigga@gmail.com>
2017-06-15 10:54:10 -0500

    Changed to explicitly disable background music

--- src/ios/BackgroundAudio.m
+++ src/ios/BackgroundAudio.m
@@ -3,18 +3,10 @@
 
 @implementation BackgroundAudio
 
-NSString* theCallbackId;
-
 // this method is executed when the app loads because of the onload param in plugin.xml
 - (void)pluginInitialize {
-  AVAudioSession *audioSession = [AVAudioSession sharedInstance];
-  NSError *setCategoryError = nil;
-  BOOL ok = [audioSession setCategory:AVAudioSessionCategoryPlayback error:&setCategoryError];
-
-  NSLog(@"BackgroundAudio plugin ok? %@", ok ? @"YES" : @"NO");
-  if (!ok) {
-    NSLog(@"BackgroundAudio plugin error: %@", setCategoryError.description);
-  }
+ [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
+ [[AVAudioSession sharedInstance] setActive: NO error: nil];
 }
 
 @end
